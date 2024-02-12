2cd06226240e0f1b2e0c95a8afcd886a72510467
NeroRT56 <nrodriguez980@soyudemedellin.edu.co>
2024-02-12 13:42:08 -0500

    varios puntos

---
+++ punto4.py
@@ -0,0 +1,5 @@
+numero=int(input("ingrese numero "))
+if numero %2==0:
+    print(numero,"es par ")
+else:
+    print(numero,"es impar")
---
+++ punto6.py
@@ -0,0 +1,3 @@
+lista=[1,2,3,4,5]
+suma=sum(lista)
+print("suma:", suma)
